01b122de86d1ead0a613b34554a66662cc345ddc
gkrishnasai16 <gkrishnasai16@gmail.com>
2020-10-10 15:44:59 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './components/HomePage';
+
+const jsx = (
+    <HomePage />
+);
+
+ReactDOM.render(jsx,document.getElementById('app'));
+
